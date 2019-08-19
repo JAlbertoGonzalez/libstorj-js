8e05550ef04b2c926c07fe4df76bf54c22ed6f6d
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-08-19 15:45:50 +0200

    Encrypt data from strings

--- test/crypto.unit.js
+++ test/crypto.unit.js
@@ -11,4 +11,16 @@ describe('# crypto functions', () => {
         const decrypted = crypto.decryptData('dummy_keypass', 'dummy@mail.com', '021e0d20f6d8d9cfd7cafe7db41910dbc0c66f79759f0bd5f2b0b05609434a246e428d0ad2b42e0622ee7a9aede65cbbab2761d4c3e90e16dab9bcd0a520f4926c39d0c1764b5c825a6760c115a1b8ebb675f772fdda11330de902a764530d853776628e31582ae0987c494ebc5db5f6b6bbbd3e43188e5215bf864eb5fa2b180b75757053849592f934b63a20');
         expect(decrypted).equals('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
     });
+
+    let encrypted = '';
+    it ('should encrypt data', () => {
+        const encrypt = require('../lib/crypto/encryptData');
+        encrypted = encrypt.encryptData('dummy_keypass', 'dummy@mail.com', 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
+        expect(encrypted).equals('021e0d20f6d8d9cfd7cafe7db41910dbc0c66f79759f0bd5f2b0b05609434a246e428d0ad2b42e0622ee7a9aede65cbbab2761d4c3e90e16dab9bcd0a520f4926c39d0c1764b5c825a6760c115a1b8ebb675f772fdda11330de902a764530d853776628e31582ae0987c494ebc5db5f6b6bbbd3e43188e5215bf864eb5fa2b180b75757053849592f934b63a20');
+    });
+
+    it('should decrypt encrypted data', () => {
+        const decrypted = crypto.decryptData('dummy_keypass', 'dummy@mail.com', encrypted);
+        expect(decrypted).equals('abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about');
+    })
 });
